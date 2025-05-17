16112d1de00d6216c60a2cb466a88de88b05b025
smabari <mdazizulbarishafin@gmail.com>
2025-05-17 13:02:18 +0600

    2nd nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className=" flex justify-center flex-col items-center gap-3 mt-5">
         <img src={logo} className="w-[400px]" alt="" />
         <p className="text-accent">Journalism Without Fear or Favour</p>
-        <p className="text-semibold text-accent">
+        <p className="font-semibold text-accent">
           {format(new Date(), "EEEE,MMMM MM, yyyy")}
         </p>
       </div>
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
     <div>
       <header>
         <Header />
+        <section className="w-11/12 mx-auto my-3">
+          <LatestNews></LatestNews>
+        </section>
+        <nav className="w-11/12 mx-auto my-3">
+          <Navbar />
+        </nav>
       </header>
       <main>
         <section className="left_nav"></section>
